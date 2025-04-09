5a72a219b4d6035460def5b5e5434269a9ffd43f
gurman007 <77499829+gurman007@users.noreply.github.com>
2025-04-09 17:05:52 -0500

    some changes in scripts, data

--- scripts/demo.py
+++
@@ -1,8 +0,0 @@
-import os
-
-print("📂 Current working directory:", os.getcwd())
-
-if os.path.exists("data/final_labeled_dataset.csv"):
-    print("✅ File found.")
-else:
-    print("❌ File NOT found in 'data/' folder.")
